cfab8ff63be2b0313585300034373de9beaab452
D-4costa <danielleelisabeth01@gmail.com>
2025-12-01 13:12:37 -0500

    Update fetchRecipes URL path to relative

--- W06 Project Completion/js/data.js
+++ W06 Project Completion/js/data.js
@@ -1,5 +1,5 @@
 // js/data.js
-export async function fetchRecipes(url = '/data/recipes.json') {
+export async function fetchRecipes(url = './data/recipes.json') {
   try {
     const res = await fetch(url);
     if (!res.ok) throw new Error(`Network error: ${res.status}`);
